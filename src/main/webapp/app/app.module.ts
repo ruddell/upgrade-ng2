cd7efc3cd1d797b15599b99768bec81f802a0731
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2017-02-16 17:29:37 -0800

    Generated with JHipster 4.0.2

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,58 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { Ng2SharedModule, UserRouteAccessService } from './shared';
+import { Ng2AdminModule } from './admin/admin.module';
+import { Ng2AccountModule } from './account/account.module';
+import { Ng2EntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { HomeComponent } from './home';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        Ng2SharedModule,
+        Ng2AdminModule,
+        Ng2AccountModule,
+        Ng2EntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        HomeComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class Ng2AppModule {}
